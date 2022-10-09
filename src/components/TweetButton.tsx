95e5e317f57b7438d2dc4bb63f9046c4791588eb
Marcus Molchany <marcusmolchany@gmail.com>
2022-10-08 22:54:22 -0700

    add suffix to tweet text

--- src/components/TweetButton.tsx
+++ src/components/TweetButton.tsx
@@ -1,9 +1,15 @@
 import { ReactElement } from "react";
+import packageJson from "../../package.json";
 import { SafeExternalLink } from "./SafeExternalLink";
+
+const { homepage } = packageJson;
 
 const getTwitterShareLink = (tweetText: string): string => {
   const twitterShareIntentBase = "https://twitter.com/intent/tweet";
-  return `${twitterShareIntentBase}?text=${encodeURI(tweetText)}`;
+  const suffix = `Signed using Wallet Sign ${homepage}`;
+  return `${twitterShareIntentBase}?text=${encodeURI(
+    `${tweetText}\n\n${suffix}`
+  )}`;
 };
 
 type Props = {
