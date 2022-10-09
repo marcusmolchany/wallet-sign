6bef8fc11c981b16819c070fc1562c491338be99
Marcus Molchany <marcusmolchany@gmail.com>
2022-10-08 20:35:22 -0700

    copy and copied button text

--- src/components/CopyButton.tsx
+++ src/components/CopyButton.tsx
@@ -1,10 +1,22 @@
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
 
 type Props = { text: string };
 
+const BUTTON_TEXT = Object.freeze({
+  COPY: "Copy",
+  COPIED: "Copied!",
+});
+
 export function CopyButton({ text }: Props): ReactElement {
+  const [buttonText, setButtonText] = useState<string>(BUTTON_TEXT.COPY);
+
   const onClick = () => {
     navigator.clipboard.writeText(text);
+    setButtonText(BUTTON_TEXT.COPIED);
+    setTimeout(() => {
+      setButtonText(BUTTON_TEXT.COPY);
+    }, 800);
   };
-  return <button onClick={onClick}>Copy</button>;
+
+  return <button onClick={onClick}>{buttonText}</button>;
 }
