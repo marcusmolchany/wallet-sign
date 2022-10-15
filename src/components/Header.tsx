02975ffa44c2cd29ffdbe92fd29c1ec1c9e654c7
Marcus Molchany <marcusmolchany@gmail.com>
2022-10-14 20:56:39 -0700

    active nav link styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,20 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./Header.css";
 
 export function Header() {
   return (
     <header className="header">
       <div className="header__links">
-        <Link to="/">
+        <NavLink to="/" end>
           <h2 className="header_h2">✍️ Wallet Sign</h2>
-        </Link>
-        <Link to="/">Sign</Link>
-        <Link to="verify">Verify</Link>
+        </NavLink>
+        <NavLink to="/" end>
+          Sign
+        </NavLink>
+        <NavLink to="verify" end>
+          Verify
+        </NavLink>
       </div>
       <ConnectButton chainStatus="none" />
     </header>
